59e935b16a688afa09710a23df4a4bae9ebaebc8
Emam96 <emamsh990@gmail.com>
2021-08-24 17:37:43 +0300

    lab 13 commit 1

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,25 +1,32 @@
 import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
+import { Jumbotron, Form, Button } from "react-bootstrap/";
 import "./BestBooks.css";
+
+import { withAuth0 } from "@auth0/auth0-react";
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: "",
       books: [],
     };
   }
 
   componentDidMount = async () => {
-    
-    
-    let url =`${process.env.REACT_APP_DATABASE}`;
+    const { user, isAuthenticated } = this.props.auth0;
+
+    await this.setState({
+      email: user.email,
+    });
+
+    let url = `${process.env.REACT_APP_DATABASE}/books?email=${this.state.email}`; 
 
     console.log(url);
-    let booksData = await axios.get(url);   // http://localhost:4000/books
-    
+    let booksData = await axios.get(url); // http://localhost:4000/books
+
     this.setState({
       books: booksData.data,
     });
@@ -27,33 +34,100 @@ class MyFavoriteBooks extends React.Component {
     console.dir(booksData.data[0].title);
   };
 
+  sendBook = async (event) => {
+    event.preventDefault();
+    
+
+    let title = event.target.title.value;
+
+    let description = event.target.description.value;
+
+    let bookDataObj = {
+      email: this.state.email,
+      title,
+      description,
+    };
+   
+    let resData = await axios.post(
+      `${process.env.REACT_APP_DATABASE}/addbook`,
+      bookDataObj
+    );
+
+    await this.setState({
+      books: resData.data,
+    });
+  };
+
+  deleteBook = async (index) => {
+    
+    let paramsObj = {
+      email: this.state.email,
+    };
+
+    let resData = await axios.delete(`${process.env.REACT_APP_DATABASE}/deletebook/${index}`, { params: paramsObj });
+
+    await this.setState({
+      books: resData.data,
+    });
+  };
+
+
+
+
+
+
   render() {
     return (
-      <div className="tab2">
-        {this.state.books.length !== 0 ? (
-          this.state.books.map((item, i) => {
-            return (
-              <Jumbotron key={i}>
-                <h1>title= {item.title}</h1>
-                <p>{item.description}</p>
-              </Jumbotron>
+      <>
 
-              // <Card style={{ width: "18rem" }}>
-              //   <Movie
-              //     key={i}
-              //     title={item.title}
-              //     poster={item.poster}
-              //     state={this.state}
-              //   />
-              // </Card>
-            );
-          })
-        ) : (
-          <p>No Books added yet</p>
-        )}
-      </div>
+<Form  onSubmit={this.sendBook}>
+          <Form.Group>
+            <Form.Control
+              className="mb-2"
+              type="text"
+              placeholder="Book name"
+              name="title"
+            />
+
+            <Form.Control
+              className="mb-2"
+              type="text"
+              placeholder="Book description"
+              name="description"
+            />
+          </Form.Group>
+          <Button type="submit"  variant="primary">
+              Add book
+            </Button>
+        </Form>
+
+
+        <div className="tab2">
+          <ul>
+          {this.state.books.length !== 0 ? (
+            this.state.books.map((item, i) => {
+              return (
+                <li key={i}>
+                  <h4> {item.title}</h4>
+                  <p>{item.description}</p>
+                  <Button variant="bottom" variant="danger" onClick={() => this.deleteBook(i)}>
+                  Delete 
+                        </Button>
+                 
+                </li>
+                
+              );
+            })
+          ) : (
+            <p>No Books added yet</p>
+          )}
+          </ul>
+        </div>
+
+        
+      </>
     );
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
