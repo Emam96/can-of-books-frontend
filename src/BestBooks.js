e73d8cab9ab19aada7382fe5b5a281ddfbcf9832
Emam96 <emamsh990@gmail.com>
2021-08-25 17:37:11 +0300

    lab 13 / delete done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {  Form, Button } from "react-bootstrap/";
+import { Form, Button } from "react-bootstrap/";
 import "./BestBooks.css";
 
 import { withAuth0 } from "@auth0/auth0-react";
@@ -15,6 +15,8 @@ class MyFavoriteBooks extends React.Component {
     };
   }
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
   componentDidMount = async () => {
     const { user, isAuthenticated } = this.props.auth0;
 
@@ -22,7 +24,7 @@ class MyFavoriteBooks extends React.Component {
       email: user.email,
     });
 
-    let url = `${process.env.REACT_APP_DATABASE}/books?email=${this.state.email}`; 
+    let url = `${process.env.REACT_APP_DATABASE}/books?email=${this.state.email}`;
 
     console.log(url);
     let booksData = await axios.get(url); // http://localhost:4000/books
@@ -34,10 +36,12 @@ class MyFavoriteBooks extends React.Component {
     console.dir(booksData.data[0].title);
   };
 
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
   sendBook = async (event) => {
     event.preventDefault();
-    
-console.log("sendBook is runnig");
+
+    console.log("sendBook is runnig");
     let title = event.target.title.value;
 
     let description = event.target.description.value;
@@ -61,32 +65,25 @@ console.log("sendBook is runnig");
     console.log("sendBook finished runnig");
   };
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  deleteBook = async (bookID) => {
 
+    let resData = await axios.delete(
+      `${process.env.REACT_APP_DATABASE}/deletebook/${bookID}?email=${this.state.email}`
+    );
 
-  deleteBook = async (id) => {
-    
-    let paramsObj = {
-      email: this.state.email,
-    };
-
-    let resData = await axios.delete(`${process.env.REACT_APP_DATABASE}/deletebook/${id}`, { params: paramsObj });
-
-    await this.setState({
+     this.setState({
       books: resData.data,
     });
   };
 
-
-
-
-
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   render() {
     return (
       <>
-
-<Form  onSubmit={this.sendBook}>
+        <Form onSubmit={this.sendBook}></Form>
           <Form.Group>
             <Form.Control
               className="mb-2"
@@ -103,35 +100,34 @@ console.log("sendBook is runnig");
             />
             {/* <input type="submit" value="Add cat" /> */}
           </Form.Group>
-          <Button type="submit"  variant="primary">
-              Add book
-            </Button>
+          <Button type="submit" variant="primary">
+            Add book
+          </Button>
         </Form>
-
 
         <div className="tab2">
           <ul>
-          {this.state.books.length !== 0 ? (
-            this.state.books.map((item, i) => {
-              return (
-                <li key={i}>
-                  <h4> {item.title}</h4>
-                  <p>{item.description}</p>
-                  <Button variant="bottom" variant="danger" onClick={() => this.deleteBook(i)}>
-                  Delete 
-                        </Button>
-                 
-                </li>
-                
-              );
-            })
-          ) : (
-            <p>No Books added yet</p>
-          )}
+            {this.state.books.length !== 0 ? (
+              this.state.books.map((item, i) => {
+                return (
+                  <li key={i}>
+                    <h4> {item.title}</h4>
+                    <p>{item.description}</p>
+                    <Button
+                      variant="bottom"
+                      variant="danger"
+                      onClick={() => this.deleteBook(item._id)}
+                    >
+                      Delete
+                    </Button>
+                  </li>
+                );
+              })
+            ) : (
+              <p>No Books added yet</p>
+            )}
           </ul>
         </div>
-
-        
       </>
     );
   }
